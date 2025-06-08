8f829a243a7378db4762eb00bc089c5f4b8d44a4
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2025-06-08 11:47:38 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import menu
+
+gambiarra = None
+while gambiarra == None:
+    gambiarra = menu.Principal(gambiarra)
--- menu.py
+++ menu.py
@@ -1,31 +1,42 @@
 import InquirerPy, DB
-from MyLibrary import Clean
+from MyLibrary import Clean, Write
 from datetime import datetime
-import re
+from time import sleep
 
-def Principal():
+Save = []
+
+def Principal(gambiarra: int):
+    Clean()
     menu = [
         {
             'type': 'list',
             'name': 'opcao',
             'message': f'\nSISTEMA: Qual ação deseja realizar?',
-            'choices': ['Registrar Entrada', 'Registrar Saida', 'Listar', 'Buscar'],
+            'choices': ['Registrar Entrada', 'Registrar Saida', 'Listar', 'Buscar', 'Encerrar'],
         }
     ]
-    Clean()
     match InquirerPy.prompt(menu):
         case {'opcao': 'Registrar Entrada'}:
             Registrar.Entrada()
         case {'opcao': 'Registrar Saida'}:
             Registrar.Saida()
         case {'opcao': 'Listar'}:
-            Listar()
-            input()
+            Exibir(Listar())
         case {'opcao': 'Buscar'}:
-            Buscar()
-            input()
+            Exibir(Buscar())
+        case {'opcao': 'Encerrar'}:
+            Clean()
+            Write("Encerrando o sistema", 0.2)
+            Write(".....", 0.2)
+            sleep(1)
+            Clean()
+            print("Sistema encerrado com sucesso!")
+            gambiarra = 0
+            return gambiarra
+        
 
 def Nome() -> str:
+    Clean()
     nome = InquirerPy.prompt([
         {
             'type': 'input',
@@ -33,10 +44,10 @@ def Nome() -> str:
             'message': 'Digite o nome do aluno:',
         }
     ])['nome']
-    Clean()
     return nome
 
 def Turma() -> str:
+    Clean()
     curso = [
         {
             'type': 'list',
@@ -47,6 +58,7 @@ def Turma() -> str:
     ]
     match InquirerPy.prompt(curso):
         case {'opcao': 'INFO'}:
+            Clean()
             turma = [
                 {
                     'type': 'list',
@@ -55,9 +67,9 @@ def Turma() -> str:
                     'choices': ['1° INFO', '2° INFO', '3° INFO'],
                 }
             ]
-            Clean()
             return InquirerPy.prompt(turma)['opcao']
         case {'opcao': 'AGRO'}:
+            Clean()
             turma = [
                 {
                     'type': 'list',
@@ -66,9 +78,9 @@ def Turma() -> str:
                     'choices': ['1° AGRO', '2° AGRO', '3° AGRO'],
                 }
             ]
-            Clean()
             return InquirerPy.prompt(turma)['opcao']
         case {'opcao': 'MINE'}:
+            Clean()
             turma = [
                 {
                     'type': 'list',
@@ -77,9 +89,9 @@ def Turma() -> str:
                     'choices': ['1° MINE', '2° MINE', '3° MINE'],
                 }
             ]
-            Clean()
             return InquirerPy.prompt(turma)['opcao']
         case {'opcao': 'ADM'}:
+            Clean()
             turma = [
                 {
                     'type': 'list',
@@ -88,24 +100,53 @@ def Turma() -> str:
                     'choices': ['1° ADM', '2° ADM', '3° ADM'],
                 }
             ]
-            Clean()
             return InquirerPy.prompt(turma)['opcao']
-    Clean()
 
 class Registrar:
     def Entrada():
+        Clean()
         nome = Nome()
         turma = Turma()
-        tipo = 'Entrada'
-        return DB.insert(nome, turma, tipo)
+        x = DB.insert(nome, turma, 'Entrada')
+        if x['success']:
+            global Save
+            Save.append({'nome': nome, 'turma': turma})
+            return x
+        else:
+            print(f"3RR0R: {x['error']}")
+            return None
 
     def Saida():
-        nome = Nome()
-        turma = Turma()
-        tipo = 'Saida'
-        return DB.insert(nome, turma, tipo)
+        Clean()
+        choices = [
+            {
+                'name': f"{visitante['nome']} - {visitante['turma']}",
+                'value': visitante 
+            }
+            for visitante in Save
+        ]
+        visitante = [
+            {
+                'type': 'list',
+                'name': 'opcao',
+                'message': 'Selecione o visitante para registrar saída:',
+                'choices': choices,
+            }
+        ]
+        opcao = InquirerPy.prompt(visitante)['opcao']
+        if opcao == 'Nenhum visitante registrado':
+            input("3RR0R: Nenhum visitante registrado! Registre uma entrada primeiro.")
+        else:
+            x = DB.insert(opcao['nome'], opcao['turma'], 'Saida')
+            if x['success']:
+                Save.remove(opcao)
+                return x
+            else:
+                print(f"3RR0R: {x['error']}")
+                return None
 
 def data(gambiarra: str) -> str:
+    Clean()
     data = None
     while not data:
         data = InquirerPy.prompt([
@@ -124,6 +165,7 @@ def data(gambiarra: str) -> str:
             data = None
 
 def hora(gambiarra: str) -> str:
+    Clean()
     hora = None
     while not hora:
         hora = InquirerPy.prompt([
@@ -141,7 +183,8 @@ def hora(gambiarra: str) -> str:
             print("3RR0R: Formato inválido! Use HH:MM:SS (00-23:00-59:00-59)")
             hora = None
 
-def Buscar():
+def Buscar() -> list:
+    Clean()
     data_inicio = data('de inicio da busca')
     hora_inicio = hora('de inicio da busca')
     data_fim = data('de fim da busca')
@@ -152,17 +195,19 @@ def Buscar():
         data_fim = data('de fim da busca')
         hora_fim = hora('de fim da busca')
     registros = DB.get_all([data_inicio, data_fim], [hora_inicio, hora_fim])
+    return registros
+
+def Listar() -> list:
+    Clean()
+    now = datetime.now()
+    registros = DB.get_all(['2025-01-01', now.date().strftime("%Y-%m-%d")], ['00:00:00', now.time().strftime("%H:%M:%S")])
+    return registros
+
+def Exibir(registros: list):
     Clean()
     if not registros:
-        print("Nenhum registro encontrado nesse período.")
+        print("Nenhum registro encontrado.")
     else:
         for registro in registros:
             print(f"Nome: {registro['nome']} - Turma: {registro['turma']} - Tipo: {registro['tipo']} - {registro['data']} às {registro['hora']}")
-
-
-def Listar():
-    now = datetime.now()
-    registros = DB.get_all(['2025-01-01', now.date().strftime("%Y-%m-%d")], ['00:00:00', now.time().strftime("%H:%M:%S")])
-    Clean()
-    for registro in registros:
-        print(f"Nome: {registro['nome']} - Turma: {registro['turma']} - Tipo: {registro['tipo']} - {registro['data']} às {registro['hora']}")
+    input("Pressione Enter para continuar...")
